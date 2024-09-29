38c2d9302f9a4c48aa20e05c6d320a616917e25f
Zombie-220 <3om6n_220@mail.ru>
2024-09-29 22:00:13 +0700

    change styles for errorpage

--- bicycle/src/navigation/errorPage/index.jsx
+++ bicycle/src/navigation/errorPage/index.jsx
@@ -9,7 +9,7 @@ export const ErrorPage = () => {
         <div className="error">
             <h1 className="error__header">404</h1>
             <p className="error__text">Страница не найдина</p>
-            <p className="error__text">Возможно страница была перенесена или её не существует</p>
+            <p className="error__text-litle">Возможно страница была перенесена или её не существует</p>
             <Link to='/' className="error__link">На главную</Link>
         </div>
     )
