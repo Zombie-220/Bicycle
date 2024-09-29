d2122e4ef17ee1a117ab80510fc31c7bcc4606b2
Zombie-220 <3om6n_220@mail.ru>
2024-09-29 17:09:42 +0700

    добавил шрифты и начал главную страницу

--- bicycle/src/pages/main/index.jsx
+++ bicycle/src/pages/main/index.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom';
+
 import './style.scss'
+import '../../assets/fonts/fonts.css'
 
 export const Main = () => {
     return (
         <div className='main'>
-            
+            <div className='main__welcomeDiv'>
+                <h1 className='main__welcomeDiv__header'>ЭЛЕКТРО<br />ВЕЛОСИПЕДЫ</h1>
+                <p className='main__welcomeDiv__text'>Cento10 Hybrid — это гоночный велосипед c помогающим<br />педалированию электроприводом, который устанавливает новый,<br />очень высокий стандарт для данной категории</p>
+                <Link className='main__welcomeDiv__link'>Подробнее</Link>
+            </div>
         </div>
     );
 };
