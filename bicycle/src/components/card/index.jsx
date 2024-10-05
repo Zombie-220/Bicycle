0aa5276a98d7f13166934e091d0a4f4827d6e6b7
Zombie-220 <3om6n_220@mail.ru>
2024-10-05 12:44:45 +0700

    backup

--- bicycle/src/components/card/index.jsx
+++ bicycle/src/components/card/index.jsx
@@ -6,6 +6,7 @@ import clickIcon from './icon/click.svg';
 
 export const Card = ({imageIMG, countryIMG, name, price, status}) => {
     const [cardStatusColor, setCardStatusColor] = useState('card__status__red');
+    const [show, setShow] = useState(false);
 
     useEffect(() => {
         if ( status === 'В наличии' ) { setCardStatusColor('card__status__green'); }
@@ -22,6 +23,7 @@ export const Card = ({imageIMG, countryIMG, name, price, status}) => {
                 <p className="card__wrapper__name">{name}</p>
                 <p className="card__wrapper__price">{price} ₽</p>
             </div>
+
             <Link className='card__link'>
                 <img src={clickIcon} alt={clickIcon} className='card__link__img'/>
                 <p className='card__link__text'>В 1 клик</p>
