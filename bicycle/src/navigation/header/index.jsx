65db1317f7c5a0017151c8418b20394382ab4038
Zombie-220 <3om6n_220@mail.ru>
2024-10-07 21:20:31 +0700

    переделал header

--- bicycle/src/navigation/header/index.jsx
+++ bicycle/src/navigation/header/index.jsx
@@ -3,11 +3,9 @@ import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../App'
 
 import icon from '../../assets/icons/icon.svg'
-import search from '../../assets/icons/header/search.svg'
 import user from '../../assets/icons/header/user.svg'
 import favorites from '../../assets/icons/header/favorites.svg'
 import cart from '../../assets/icons/header/cart.svg'
-import rightMenu from '../../assets/icons/header/rightMenu.svg'
 
 import './style.scss'
 
@@ -26,19 +24,17 @@ export const Header = () => {
         <header className='header'>
             <Link className='header__link'> <img src={icon} alt="logo" /> </Link>
             <div className='header__wrapper'>
-                {isAuth && (<Link to={'/tradeIn'} className='header__link link_orange'>TRADE IN</Link>)}
-                {isAuth && (<Link to={'/bicycle'} className='header__link'>ВЕЛОСИПЕДЫ</Link>)}
-                {isAuth && (<Link to={'/'} className='header__link'>ЗАПЧАСТИ</Link>)}
-                {isAuth && (<Link to={'/'} className='header__link'>АКСЕССУАРЫ</Link>)}
-                {isAuth && (<Link to={'/'} className='header__link'>ВЕЛОСТАНКИ</Link>)}
-                {isAuth && (<button className='header__button search_img'> <img src={search} alt="searchImg" /> </button>)}
-                {!isAuth && (<Link to={'/auth'} className='header__link'> <img src={user} alt="user" /> </Link>)}
-                {isAuth && (<Link to={'/'} className='header__button'> <img src={favorites} alt="favorites" /> </Link>)}
-                {isAuth && (<Link to={'/'} className='header__button'> <img src={cart} alt="cart" /> </Link>)}
-                {!isAuth && (<Link to={'/'} className='header__button'> <img src={rightMenu} alt="rightMenu" /> </Link>)}
-                {isAuth && (<button className='quitButton' onClick={logout}>quit</button>)}
+                <Link to={'/bicycle'} className='header__link'>ВЕЛОСИПЕДЫ</Link>
+                <Link to={'/parts'} className='header__link'>ЗАПЧАСТИ</Link>
+                <Link to={'/gears'} className='header__link'>АКСЕССУАРЫ</Link>
+                <Link to={'/bikeRacks'} className='header__link'>ВЕЛОСТАНКИ</Link>
+                {!isAuth && (<Link to={'/auth'} className='header__link'>ВОЙТИ</Link>)}
+                {!isAuth && (<Link to={'/register'} className='header__link link_orange'>РЕГИСТРАЦИЯ</Link>)}
+                {isAuth && (<Link to={'/user'} className='header__link'> <img src={user} alt="user" /> </Link>)}
+                {isAuth && (<Link to={'/favorites'} className='header__button'> <img src={favorites} alt="favorites" /> </Link>)}
+                {isAuth && (<Link to={'/cart'} className='header__button'> <img src={cart} alt="cart" /> </Link>)}
+                {isAuth && (<button className='header__link link_orange' onClick={logout}>ВЫЙТИ</button>)}
             </div>
         </header>
     );
 };
-
