7efb8143f9380d1d15d6ab2ab3a581bdb87397b9
Zombie-220 <3om6n_220@mail.ru>
2024-10-08 23:47:46 +0700

    удалил пробелы

--- bicycle/src/pages/auth/index.jsx
+++ bicycle/src/pages/auth/index.jsx
@@ -37,7 +37,6 @@ export const Auth = () => {
                     <button className="auth__wrapper__header__login">Войти</button>
                     <Link to={'/register'} className="auth__wrapper__header__signup">Регистрация</Link>
                 </div>
-
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <ValidateInput
                         textLabel={"Имя пользователя"}
@@ -59,11 +58,6 @@ export const Auth = () => {
                     />
                     <button className="auth__wrapper__enterButton">Войти</button>
                 </form>
-
-                {/* <p className="auth__wrapper__text">Имя пользователя</p>
-                <input type="text" className="auth__wrapper__input" onChange={(elem) => { setUserName(elem.target.value) }}/> */}
-                {/* <p className="auth__wrapper__text">Пароль</p>
-                <input type="password" className="auth__wrapper__input" onChange={(elem) => { setUserPassword(elem.target.value) }}/> */}
                 <div className="auth__wrapper__lowerBox">
                     <input type="checkbox" className="auth__wrapper__lowerBox__checkbox"/>
                     <p className="auth__wrapper__lowerBox__text">Запомнить меня</p>
