854978b93ce82a6a06aeeae20333083ef5eaa374
Zombie-220 <3om6n_220@mail.ru>
2024-10-13 19:19:20 +0700

    вернул (временно) картинки для карточек

--- bicycle/src/components/card/index.jsx
+++ bicycle/src/components/card/index.jsx
@@ -5,7 +5,10 @@ import { useCallback } from 'react';
 import './style.scss';
 import clickIcon from './icon/click.svg';
 
-export const Card = ({imageIMG, countryIMG, name, price, status, id, onEdit, onRemove}) => {
+import bicycle from './temp_bicycle.png';
+import country from './temp_country.png';
+
+export const Card = ({bicycleIMG, countryIMG, name, price, status, id, onEdit, onRemove}) => {
     const [cardStatusColor, setCardStatusColor] = useState('card__status__red');
 
     const removeProduct = useCallback(() => {
@@ -23,11 +26,11 @@ export const Card = ({imageIMG, countryIMG, name, price, status, id, onEdit, onR
     return (
         <div className="card">
             <div className='card__relativeDiv'>
-                <img src={countryIMG} alt="countryIMG" className='card__relativeDiv__country'/>
+                <img src={country} alt="countryIMG" className='card__relativeDiv__country'/>
                 <p className={`card__relativeDiv__status ${cardStatusColor}`}>{status}</p>
             </div>
             <div className='card__wrapper'>
-                <img src={imageIMG} alt="bicycleIMG" className="card__wrapper__image"/>
+                <img src={bicycle} alt="bicycleIMG" className="card__wrapper__image"/>
                 <p className="card__wrapper__name">{name}</p>
                 <p className="card__wrapper__price">{price} ₽</p>
             </div>
