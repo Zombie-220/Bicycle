c26eab463a1db9232bd18511cd7ef119e60f5a13
Zombie-220 <3om6n_220@mail.ru>
2024-12-23 13:30:28 +0700

    header теперь сам красится в зависимости от страницы

--- bicycle/src/navigation/header/index.jsx
+++ bicycle/src/navigation/header/index.jsx
@@ -1,9 +1,8 @@
-import { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import { AuthContext, AdminContext } from '../../App';
 import { ModalWindow } from '../../components/ModalWindow/ModalWindow';
-import { AuthModalWindow } from '../../components/ModalWindow/AuthModalWindow';
 import { RegisterModalWindow } from '../../components/ModalWindow/RegisterModalWindow';
 
 import icon from '../../assets/icons/icon.svg';
@@ -16,10 +15,11 @@ export const Header = () => {
     const { isAuth, setIsAuth } = useContext(AuthContext);
     const { isAdmin, setIsAdmin } = useContext(AdminContext);
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [authIsOpen, setAuthIsOpen] = useState(false);
     const [registerIsOpen, setRegisterIsOpen] = useState(false);
+    const [background, setBackground] = useState("#0000");
 
     const navigate = useNavigate();
+    const location = useLocation();
   
     const logout = () => {
         navigate("/");
@@ -28,18 +28,19 @@ export const Header = () => {
         setIsAdmin(false);
     };
 
-    const openAuthModal = () => {
-        setModalIsOpen(false);
-        setAuthIsOpen(true);
-    };
+    useEffect(() => {
+        console.log(location)
+        if (location.pathname !== '/') { setBackground('#101010'); }
+        else { setBackground('#0000'); }
+    }, [location]);
 
     return (
-        <header className='header'>
+        <header className='header' style={{background: background}}>
             <Link className='header__link'> <img src={icon} alt="logo" /> </Link>
             <div className='header__wrapper'>
                 {isAdmin && (<Link to={'/admin/bicycle'} className='header__button link_orange secondAdaptive'>Админ</Link>)}
                 <Link to={'/bicycle'} className='header__link adaptive'>ВЕЛОСИПЕДЫ</Link>
-                {isAuth === '' && (<button className='header__link secondAdaptive' onClick={()=>{setAuthIsOpen(true)}}>ВОЙТИ</button>)}
+                {isAuth === '' && (<Link to={'/auth'} className='header__link secondAdaptive'>ВОЙТИ</Link>)}
                 {isAuth === '' && (<button className='header__link link_orange secondAdaptive' onClick={()=>{setRegisterIsOpen(true)}}>РЕГИСТРАЦИЯ</button>)}
                 {isAuth !== '' && (<Link to={'/cart'} className='header__button secondAdaptive'> <img src={cart} alt="cart" /> </Link>)}
                 {isAuth !== '' && (<button className='header__link link_orange secondAdaptive' onClick={logout}>ВЫЙТИ</button>)}
@@ -50,14 +51,13 @@ export const Header = () => {
                 <p className='header__burgerMenuWrapper__header'>МЕНЮ</p>
                     {isAdmin && (<Link to={'/admin/bicycle'} className='header__button link_orange'>Админ</Link>)}
                     <Link to={'/bicycle'} className='header__burgerMenuWrapper__link'>ВЕЛОСИПЕДЫ</Link>
-                    {isAuth === '' && (<button className='header__burgerMenuWrapper__link' onClick={()=>{setAuthIsOpen(true); setModalIsOpen(false)}}>ВОЙТИ</button>)}
+                    {isAuth === '' && (<Link to={'/auth'} className='header__link secondAdaptive'>ВОЙТИ</Link>)}
                     {isAuth === '' && (<button className='header__burgerMenuWrapper__link link_orange' onClick={()=>{setRegisterIsOpen(true); setModalIsOpen(false)}}>РЕГИСТРАЦИЯ</button>)}
                     {isAuth !== '' && (<Link to={'/cart'} className='header__burgerMenuWrapper__link'>КОРЗИНА</Link>)}
                     {isAuth !== '' && (<button className='header__burgerMenuWrapper__link' onClick={logout}>ВЫЙТИ</button>)}
                 </div>
             </ModalWindow>
             <RegisterModalWindow isOpen={registerIsOpen} onClose={() => {setRegisterIsOpen(false)}} />
-            <AuthModalWindow isOpen={authIsOpen} onClose={() => {setAuthIsOpen(false)}} />
         </header>
     );
 };
