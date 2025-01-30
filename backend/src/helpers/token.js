968dce29fa4460efec388100726ee75ed2e3c178
Zombie-220 <3om6n_220@mail.ru>
2025-01-30 23:08:28 +0700

    перебераю токен

--- backend/src/helpers/token.js
+++ backend/src/helpers/token.js
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken';
-import { ObjectId } from 'mongodb';
 
 import { TOKEN_KEY } from '../config/env.js';
-import { GetUserInfo } from '../models/users.js';
 
 /**
  * Функция по созданию токена, включающего в себя:
- * - name - имя пользователя
- * - id - ID пользователя
- * @param {ObjectId} id ID пользоваиеля
+ * @param {string} id ID пользователя
+ * @param {string[]} roles роли пользователя
+ * @param {number} expires срок жизни токена
  * @returns {Promise<string>}
 */
-export const CreateToken = async (id) => {
-    const userInfo = await GetUserInfo(id);
-    const token = jwt.sign({ name: userInfo.name, id: userInfo._id }, TOKEN_KEY, { expiresIn: 168 });
+export const CreateToken = async (id, roles, expires) => {
+    const token = jwt.sign({
+        id: `${id}`,
+        roles: roles
+    }, TOKEN_KEY, { expiresIn: `${expires}hr` });
+    
     return (token);
 }
